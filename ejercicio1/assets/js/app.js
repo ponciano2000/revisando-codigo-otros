95490da7c3b5a80e198a1af818b0a6b97f20780d
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-10-28 14:04:48 -0500

    ejercicios 123

---
+++ ejercicio1/assets/js/app.js
@@ -0,0 +1,24 @@
+//Agregé /users
+const baseEndpoint = 'https://api.github.com/users';
+const usersEndpoint = `${baseEndpoint}/users`;
+
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
+
+function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
